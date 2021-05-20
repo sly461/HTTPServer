2a830fe8194ac8d3576037df22554e11ce09da7d
sly461 <857165109@qq.com>
2021-05-21 00:01:08 +0800

    for test

--- main.cpp
+++ main.cpp
@@ -3,13 +3,12 @@
 
 int main(int argc, char *argv[]) {
 
-    // if (argc <= 1)
-    // {
-    //     printf("Parameter format：server port\n");
-    //     return 0;
-    // }
-    // short port = atoi(argv[1]);
-    short port = 5555;
+    if (argc <= 1)
+    {
+        printf("Parameter format：server port\n");
+        return 0;
+    }
+    short port = atoi(argv[1]);
     
     HTTPServer server(port, -1, 8);
 
